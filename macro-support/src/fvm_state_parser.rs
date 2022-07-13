860344cb6e454a67183c68ce6226a49c66720312
tchataigner <tom.chataigner@yahoo.fr>
2022-07-13 15:19:48 +0200

    :white_check_mark: Added tests on fvm_state parser

--- macro-support/src/fvm_state_parser.rs
+++ macro-support/src/fvm_state_parser.rs
@@ -24,7 +24,7 @@ impl<'a> ConvertToAst for &'a mut syn::ItemStruct {
         if !self.generics.params.is_empty() {
             bail_span!(
                 self.generics,
-                "structs with #[fvm_state] cannot have lifetime or type parameters currently"
+                "structs with #[fvm_state] cannot have lifetime or type parameters"
             );
         }
 
@@ -87,5 +87,118 @@ impl<'a> MacroParse<&'a mut TokenStream> for syn::Item {
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quote::quote;
+    use syn::{Member, Type};
+
+    use super::*;
+
+    #[test]
+    fn struct_to_ast() {
+        let mut token_stream = TokenStream::new();
+
+        (quote! {
+            pub struct MockStruct {
+                pub count: u64
+            }
+        })
+        .to_tokens(&mut token_stream);
+        let item = syn::parse2::<syn::Item>(token_stream).unwrap();
+
+        let mut tokens = TokenStream::new();
+        let mut program = backend::ast::Program::default();
+
+        item.macro_parse(&mut program, &mut tokens).unwrap();
+
+        let parsed_struct = &program.structs[0];
+        let parsed_field = &parsed_struct.fields[0];
+
+        assert_eq!(parsed_struct.name, "MockStruct");
+        assert_eq!(parsed_struct.rust_name.to_string(), parsed_struct.name);
+
+        assert_eq!(parsed_field.name, "count");
+        assert_eq!(parsed_field.struct_name.to_string(), parsed_struct.name);
+        match &parsed_field.ty {
+            Type::Path(type_path) => {
+                assert_eq!(
+                    type_path.path.segments.last().unwrap().ident.to_string(),
+                    "u64"
+                );
+            }
+            _ => {
+                panic!("count type should be path")
+            }
+        }
+        match &parsed_field.rust_name {
+            Member::Named(ident) => {
+                assert_eq!(ident.to_string(), parsed_field.name);
+            }
+            _ => panic!("parsed struct field rust name should be named"),
+        }
+    }
+
+    #[test]
+    fn no_struct_with_lifetime() {
+        let mut token_stream = TokenStream::new();
+
+        (quote! {
+            struct MockStruct<'a> {
+                pub count: &'a u64,
+            }
+        })
+        .to_tokens(&mut token_stream);
+        let item = syn::parse2::<syn::Item>(token_stream).unwrap();
+
+        let mut tokens = TokenStream::new();
+        let mut program = backend::ast::Program::default();
+
+        let res_parse = item.macro_parse(&mut program, &mut tokens);
+
+        match res_parse {
+            Err(diagnostic) => {
+                let res_panic = std::panic::catch_unwind(|| diagnostic.panic());
+                match res_panic {
+                    Err(err) => {
+                        match err.downcast::<String>() {
+                            Ok(panic_msg_box) => {
+                                assert_eq!(panic_msg_box.as_str(), "structs with #[fvm_state] cannot have lifetime or type parameters");
+                            }
+                            Err(_) => unreachable!(),
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            _ => panic!("parse result should be error when struct has lifetime"),
+        }
+    }
+
+    #[test]
+    fn private_fields_not_parsed() {
+        let mut token_stream = TokenStream::new();
+
+        (quote! {
+            pub struct MockStruct {
+                pub count: u64,
+                count_bis: u64
+            }
+        })
+        .to_tokens(&mut token_stream);
+        let item = syn::parse2::<syn::Item>(token_stream).unwrap();
+
+        let mut tokens = TokenStream::new();
+        let mut program = backend::ast::Program::default();
+
+        item.macro_parse(&mut program, &mut tokens).unwrap();
+
+        let parsed_struct = &program.structs[0];
+        let parsed_field = &parsed_struct.fields[0];
+
+        assert_eq!(parsed_struct.fields.len(), 1usize);
+        assert_eq!(parsed_field.name, "count");
     }
 }
