64427d3d0beb2dab90a7d745d1b30c8b50f5504d
tchataigner <tom.chataigner@yahoo.fr>
2022-07-13 17:13:14 +0200

    :construction: Adding serde to structs & added test

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,9 +1,9 @@
 //! Backend crate for the Filecoin Virtual Machine Rust SDK procedural macro.
 
 pub use crate::error::Diagnostic;
-pub use crate::state_codegen::TryToTokens as TryStateToTokens;
+pub use crate::fvm_state_codegen::TryToTokens as TryStateToTokens;
 
 #[macro_use]
 mod error;
 pub mod ast;
-mod state_codegen;
+mod fvm_state_codegen;
