6bbf77caef5d5da4dd17d783c9f1832fb125a3d3
tchataigner <tom.chataigner@yahoo.fr>
2022-07-13 18:13:04 +0200

    :construction: Serde & serde_tuple as deps + exposing them

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod internal;
 
 pub use fvm_rs_sdk_macro::fvm_state;
-pub use {fvm_sdk as syscall, fvm_shared as shared};
+pub use {fvm_sdk as syscall, fvm_shared as shared, serde, serde_tuple};
 
 pub fn hello_world() -> &'static str {
     "Hello World!"
