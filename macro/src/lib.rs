885b77d3bc1c3ffcc0f9cac663d34999dbd761a4
tchataigner <tom.chataigner@yahoo.fr>
2022-07-13 08:25:42 +0200

    :construction: Fixed compilation errors

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -7,7 +7,7 @@ use quote::quote;
 
 #[proc_macro_attribute]
 pub fn fvm_state(attr: TokenStream, input: TokenStream) -> TokenStream {
-    match fvm_rs_sdk_macro_support::expand_state(attr.into, input.into()) {
+    match fvm_rs_sdk_macro_support::expand_state(attr.into(), input.into()) {
         Ok(tokens) => tokens.into(),
         Err(diagnostic) => (quote! { #diagnostic }).into(),
     }
