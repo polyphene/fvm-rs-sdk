3478447ae99d812fd599bda6030e09e087c5ebba
tchataigner <tom.chataigner@yahoo.fr>
2022-07-15 17:11:19 +0200

    :construction: Restore save & load code

--- backend/src/ast.rs
+++ backend/src/ast.rs
@@ -34,7 +34,81 @@ impl StateStruct {
         match self.codec {
             Codec::DagCbor => {
                 let name = &self.rust_name;
-                quote!()
+                quote!(
+                    impl fvm_rs_sdk::state::StateObject for #name {
+                        fn load() -> Self {
+                            use fvm_rs_sdk::serde;
+                            use fvm_rs_sdk::state::Blockstore;
+
+                            // First, load the current state root.
+                            let root = match fvm_rs_sdk::syscall::sself::root() {
+                                Ok(root) => root,
+                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
+                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
+                                    Some(format!("failed to get root: {:?}", err).as_str()),
+                                ),
+                            };
+
+                            // Get state's bytes
+                            match fvm_rs_sdk::state::cbor::CborBlockstore.get(&root) {
+                                // State fetched, convert byte to actual struct
+                                Ok(Some(state_bytes)) => match fvm_rs_sdk::encoding::from_slice(&state_bytes) {
+                                    Ok(state) => state,
+                                    Err(err) => fvm_rs_sdk::syscall::vm::abort(
+                                        fvm_rs_sdk::shared::error::ExitCode::USR_SERIALIZATION.value(),
+                                        Some(format!("failed to deserialize state: {}", err).as_str()),
+                                    )
+                                },
+                                // No state
+                                Ok(None) => fvm_rs_sdk::syscall::vm::abort(
+                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
+                                    Some("state does not exist"),
+                                ),
+                                // Fetching failed
+                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
+                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
+                                    Some(format!("failed to get state: {}", err).as_str()),
+                                ),
+                            }
+                        }
+
+                        fn save(&self) -> fvm_rs_sdk::cid::Cid {
+                            use fvm_rs_sdk::serde;
+                            use fvm_rs_sdk::state::Blockstore;
+
+                            // Serialize state
+                            let serialized = match fvm_rs_sdk::encoding::to_vec(self) {
+                                Ok(s) => s,
+                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
+                                    fvm_rs_sdk::shared::error::ExitCode::USR_SERIALIZATION.value(),
+                                    Some(format!("failed to serialize state: {:?}", err).as_str()),
+                                ),
+                            };
+
+                            // Put state
+                            let cid = match fvm_rs_sdk::state::cbor::CborBlockstore.put(
+                                fvm_rs_sdk::cid::Code::Blake2b256.into(),
+                                &fvm_rs_sdk::state::Block {
+                                    codec: fvm_rs_sdk::encoding::DAG_CBOR,
+                                    data: serialized.as_slice()
+                                }
+                            ) {
+                                Ok(cid) => cid,
+                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
+                                    fvm_rs_sdk::shared::error::ExitCode::USR_SERIALIZATION.value(),
+                                    Some(format!("failed to store initial state: {:}", err).as_str()),
+                                ),
+                            };
+                            if let Err(err) = fvm_rs_sdk::syscall::sself::set_root(&cid) {
+                                fvm_rs_sdk::syscall::vm::abort(
+                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
+                                    Some(format!("failed to set root cid: {:}", err).as_str()),
+                                );
+                            }
+                            cid
+                        }
+                    }
+                )
             }
         }
     }
