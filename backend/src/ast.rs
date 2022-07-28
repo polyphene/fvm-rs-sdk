3ca8abaeb02be8b06c6b304668fb6978b3af8dc6
tchataigner <tom.chataigner@yahoo.fr>
2022-07-29 00:04:34 +0200

    :recycle: Codegen only in dedicated file

--- backend/src/ast.rs
+++ backend/src/ast.rs
@@ -4,7 +4,7 @@
 use crate::actor::attrs::Dispatch;
 use crate::export::attrs::Binding;
 use proc_macro2::{Ident, TokenStream};
-use quote::{quote, ToTokens};
+use quote::ToTokens;
 use syn;
 
 use crate::state::attrs::Codec;
@@ -44,74 +44,6 @@ pub struct StateStruct {
     pub fields: Vec<StateStructField>,
     /// Codec used to store state
     pub codec: Codec,
-}
-
-impl TryToTokens for StateStruct {
-    fn try_to_tokens(&self, into: &mut TokenStream) -> Result<(), Diagnostic> {
-        match self.codec {
-            Codec::DagCbor => {
-                let name = &self.rust_name;
-                quote!(
-                    impl fvm_rs_sdk::state::StateObject for #name {
-                        fn load() -> Self {
-                            use fvm_rs_sdk::encoding::CborStore;
-                            // First, load the current state root.
-                            let root = match fvm_rs_sdk::syscall::sself::root() {
-                                Ok(root) => root,
-                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
-                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
-                                    Some(format!("failed to get root: {:?}", err).as_str()),
-                                ),
-                            };
-
-                            // Load the actor state from the state tree.
-                            match fvm_rs_sdk::state::cbor::CborBlockstore.get_cbor::<Self>(&root) {
-                                Ok(Some(state)) => state,
-                                Ok(None) => fvm_rs_sdk::syscall::vm::abort(
-                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
-                                    Some("state does not exist"),
-                                ),
-                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
-                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
-                                    Some(format!("failed to get state: {}", err).as_str()),
-                                ),
-                            }
-                        }
-
-                        fn save(&self) -> fvm_rs_sdk::cid::Cid {
-                            let serialized = match fvm_rs_sdk::encoding::to_vec(self) {
-                                Ok(s) => s,
-                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
-                                    fvm_rs_sdk::shared::error::ExitCode::USR_SERIALIZATION.value(),
-                                    Some(format!("failed to serialize state: {:?}", err).as_str()),
-                                ),
-                            };
-                            let cid = match fvm_rs_sdk::syscall::ipld::put(
-                                fvm_rs_sdk::cid::Code::Blake2b256.into(),
-                                32,
-                                fvm_rs_sdk::encoding::DAG_CBOR,
-                                serialized.as_slice(),
-                            ) {
-                                Ok(cid) => cid,
-                                Err(err) => fvm_rs_sdk::syscall::vm::abort(
-                                    fvm_rs_sdk::shared::error::ExitCode::USR_SERIALIZATION.value(),
-                                    Some(format!("failed to store initial state: {:}", err).as_str()),
-                                ),
-                            };
-                            if let Err(err) = fvm_rs_sdk::syscall::sself::set_root(&cid) {
-                                fvm_rs_sdk::syscall::vm::abort(
-                                    fvm_rs_sdk::shared::error::ExitCode::USR_ILLEGAL_STATE.value(),
-                                    Some(format!("failed to set root cid: {:}", err).as_str()),
-                                );
-                            }
-                            cid
-                        }
-                    }
-                ).to_tokens(into);
-            }
-        }
-        Ok(())
-    }
 }
 
 /// The field of a struct
