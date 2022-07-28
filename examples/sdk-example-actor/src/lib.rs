a7bc171e604655be7bf2d4da87e26517167f5347
tchataigner <tom.chataigner@yahoo.fr>
2022-07-29 00:46:29 +0200

    :construction: Started code generation

--- examples/sdk-example-actor/src/lib.rs
+++ examples/sdk-example-actor/src/lib.rs
@@ -1,7 +1,4 @@
-use fvm_rs_sdk::encoding::{to_vec, RawBytes, DAG_CBOR};
-use fvm_rs_sdk::shared::error::ExitCode;
-use fvm_rs_sdk::syscall::{message::params_raw, vm::abort, NO_DATA_BLOCK_ID};
-
+use fvm_rs_sdk::actor::{fvm_actor, fvm_export};
 use fvm_rs_sdk::state::*;
 
 #[derive(Clone, Debug, Default)]
@@ -10,60 +7,20 @@ pub struct State {
     pub value: u64,
 }
 
-#[no_mangle]
-pub fn invoke(params_pointer: u32) -> u32 {
-    // Conduct method dispatch. Handle input parameters and return data.
-    let ret: Option<RawBytes> = match fvm_rs_sdk::syscall::message::method_number() {
-        // Set initial value
-        1 => {
-            let params = params_raw(params_pointer).unwrap().1;
-            let x: u64 = RawBytes::new(params).deserialize().unwrap();
+#[fvm_actor]
+impl State {
+    #[fvm_export(binding = 1)]
+    pub fn new() -> Self {
+        State { value: 0 }
+    }
 
-            let mut state = State::load();
-            state.value = x;
-            state.save();
+    #[fvm_export(binding = 2)]
+    pub fn add(&mut self, value: u64) {
+        self.value += value
+    }
 
-            None
-        }
-        // Add value
-        2 => {
-            let params = params_raw(params_pointer).unwrap().1;
-            let x: u64 = RawBytes::new(params).deserialize().unwrap();
-
-            let mut state = State::load();
-            state.value += x;
-            state.save();
-
-            None
-        }
-        // Get state value
-        3 => {
-            let state = State::load();
-            let ret = to_vec(&state.value);
-            match ret {
-                Ok(ret) => Some(RawBytes::new(ret)),
-                Err(err) => {
-                    abort(
-                        ExitCode::USR_ILLEGAL_STATE.value(),
-                        Some(format!("failed to serialize return value: {:?}", err).as_str()),
-                    );
-                }
-            }
-        }
-        _ => abort(
-            ExitCode::USR_UNHANDLED_MESSAGE.value(),
-            Some("unrecognized method"),
-        ),
-    };
-
-    match ret {
-        None => NO_DATA_BLOCK_ID,
-        Some(v) => match fvm_rs_sdk::syscall::ipld::put_block(DAG_CBOR, v.bytes()) {
-            Ok(id) => id,
-            Err(err) => abort(
-                ExitCode::USR_SERIALIZATION.value(),
-                Some(format!("failed to store return value: {}", err).as_str()),
-            ),
-        },
+    #[fvm_export(binding = 3)]
+    pub fn read(&self, value: u64) -> u64 {
+        self.value
     }
 }
