5cf75cc4c77af5e1745e9f124060d3072994dc86
tchataigner <tom.chataigner@yahoo.fr>
2022-08-12 17:01:11 +0200

    :green_heart: Fixed clippy ci

--- backend/src/export/attrs.rs
+++ backend/src/export/attrs.rs
@@ -30,7 +30,7 @@ impl Parse for ExportAttr {
         let attr: AnyIdent = input.parse()?;
         let attr = attr.0;
 
-        return match ExportAttr::try_from(attr.to_string()) {
+        match ExportAttr::try_from(attr.to_string()) {
             Ok(ExportAttr::Binding(_)) => {
                 input.parse::<syn::token::Eq>()?;
                 // Try to get value from parsing an integer
@@ -44,7 +44,7 @@ impl Parse for ExportAttr {
                 return Err(original.error(format!("{}", InvalidBindingValue)));
             }
             Err(err) => Err(original.error(format!("{}", err))),
-        };
+        }
     }
 }
 
